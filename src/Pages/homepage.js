bb56b61233d4d7f296161751f42430bb716b50d5
Emiliano Fernández Hernández <emilianofh02@gmail.com>
2020-05-30 02:18:11 -0600

    Agregando Layout y homepage

---
+++ src/Pages/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/homepage.css'
+
+class homepage extends React.Component {
+    render() {
+        return(
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default homepage;
