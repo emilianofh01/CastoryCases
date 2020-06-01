1157904d7044bf71d77c64d85d5517d38431d898
JChristianDuarte <ChristianDuarte@users.noreply.github.com>
2020-05-31 22:27:27 -0600

    Añadida pagina de test

---
+++ public/test/PhoneCanvas.js
@@ -0,0 +1,20 @@
+import React from 'react';
+class PhoneCanvas extends React.Component{
+
+    constructor(props){
+        this.state = {
+            mask: "",
+            case: "",
+            widgets: []
+        }
+    }
+
+    render(){
+        return (
+            <canvas width="700" height="700"></canvas>
+        )
+    }
+
+}
+
+export default PhoneCanvas;
