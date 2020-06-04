f4c8ce8cff1b61bb1f4eeff345731fd668cdd948
Emiliano Fernández Hernández <emilianofh02@gmail.com>
2020-06-03 19:19:59 -0600

    Se agrego JQuery

--- src/Pages/homepage.js
+++ src/Pages/homepage.js
@@ -5,7 +5,8 @@ class homepage extends React.Component {
     render() {
         return(
             <div className="pages homepage_container">
-                <span className="icon-paint-brush-solid icon"></span>
+                <span className="icon-paint-brush-solid" id="iconBrush"></span>
+                <span className="icon-paint-brush-solid" id="iconCatalogue"></span>
             </div>
         )
     }
