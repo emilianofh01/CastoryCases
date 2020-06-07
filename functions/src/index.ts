65c99857d74357c83cd15d5470b15a6e469528fc
JChristianDuarte <ChristianDuarte@users.noreply.github.com>
2020-06-06 22:49:53 -0600

    Devany come cereal

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,32 +1,36 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin'
+import * as admin from 'firebase-admin';
+
 
 //const admin = require('firebase-admin');
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 //
 const app : admin.app.App = admin.initializeApp();
 const db : admin.firestore.Firestore = admin.firestore(app);
+const sharp = require('sharp');
 
 export const getProduct = functions.https.onRequest((request, response) => {
   response.set('Access-Control-Allow-Origin', '*');
   response.set('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
   response.set('Access-Control-Allow-Headers', '*');
-  let productRef : admin.firestore.DocumentReference = db.collection('products').doc('HXBF4Hi0kZd1UqqDfs4a');
+  const query = request.params[0].replace('/', '').replace('\\', '');
+  if(query == "")
+    response.send(undefined)
+  let productRef : admin.firestore.DocumentReference = db.collection('products').doc(query);
   productRef.get()
     .then(doc => {
       if (!doc.exists) {
         console.log('No such document!');
       } else{
-        console.log(`${doc.data()}`)
-        let jsonString : string = `${doc.data()}`
-        let object = JSON.parse(jsonString)
-        object.id = doc.id;
+        let object = doc.data() ?? {};
+        if(object != {}) object.id = doc.id;
         response.send(object)
       }
     })
     .catch(err => {
       console.log('Error getting document', err);
+      response.send(undefined)
     });
 });
 
@@ -38,9 +42,131 @@ export const addProduct = functions.https.onRequest((request, response)=>{
   const obj = JSON.parse(query)
   db.collection('products').add(obj).then(()=>{
     response.send("")
-  }).catch(e=>{console.log(e); response.send("Error :" + e)})
+  }).catch(e=>{console.log(e); response.send(undefined)})
 
 })
+
+function resize(imgBase64: string, width: number) : Promise<string>{
+  let parts = imgBase64.split(';');
+  let mimType = parts[0].split(':')[1];
+  let imageData = parts[1].split(',')[1];
+  var img:Buffer = new Buffer(imageData, 'base64');
+  
+  return new Promise(
+    (resolve, reject)=>{
+      sharp(img)
+            .resize({ width: width })
+            .toBuffer()
+            .then((resizedImageBuffer:any )=> {
+                let resizedImageData = resizedImageBuffer.toString('base64');
+                let resizedBase64 = `data:${mimType};base64,${resizedImageData}`;
+
+                resolve(resizedBase64);
+            })
+    }
+  )
+}
+
+export const quicksearch = functions.https.onRequest((request, response)=>{
+  response.set('Access-Control-Allow-Origin', '*');
+  response.set('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
+  response.set('Access-Control-Allow-Headers', '*');
+  const busqueda = request.params[0].replace('/', '').replace('\\', '');
+  const removeAccents = (str : string) => str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  const lowerCasedFilter = removeAccents(busqueda.toLowerCase());
+  let DatosFiltrados : any[] = []
+  db.collection('products').get().then(
+    (snapshot)=>{
+      new Promise(
+        (resolve, reject)=>{
+          let promises: Promise<void>[] = []
+          snapshot.docs.forEach((
+            (doc) =>{
+              let data = doc.data();
+              
+              if(removeAccents(data.productName).toLowerCase().includes(lowerCasedFilter)){
+                let resObj = {id: doc.id, productBrand: data.productBrand, productImages: [""], productModel: data.productModel, productName: data.productName, productPrice: data.productPrice}
+                promises.push(
+                  resize(data.productImages[0], 480).then(
+                    (img)=>{
+                      resObj.productImages[0] = img
+                      DatosFiltrados.push(resObj)
+                    }
+                  ).catch((e)=>{console.log(e); response.send(undefined);})
+                )
+                
+              }
+            }
+          ))
+          Promise.all(promises).then(
+            (e)=>resolve()
+          ).catch(e=>{console.log(e); response.send(undefined)});
+        }
+      ).then(
+        () => response.send(DatosFiltrados)
+      ).catch(e=>{console.log(e); response.send(undefined)});
+    }
+  ).catch(e=>{console.log(e); response.send(undefined)})
+
+})
+
+export const searchfilter = functions.https.onRequest((request, response)=>{
+  response.set('Access-Control-Allow-Origin', '*');
+  response.set('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
+  response.set('Access-Control-Allow-Headers', '*');
+  if(request.params[0].replace("/", "").replace("\\", "") == "")
+    response.send(undefined)
+  const busqueda = JSON.parse(request.params[0].replace("/", "").replace("\\", ""));
+  
+  const removeAccents = (str : string) => str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  const lowerCasedFilter = busqueda.productSearch == undefined ? ("") : removeAccents(busqueda.productSearch.toLowerCase());
+
+  let filters: any[] = Object.keys(busqueda);
+  let DatosFiltrados : any[] = []
+  db.collection('products').get().then(
+    (snapshot)=>{
+      new Promise(
+        (resolve, reject)=>{
+          let promises: Promise<void>[] = []
+          snapshot.docs.forEach((
+            (doc) =>{
+              let data = doc.data();
+              if(filter(data, filters, busqueda) && (!busqueda.productSearch || removeAccents(data.productName).toLowerCase().includes(lowerCasedFilter)) && (!busqueda.priceRange || (busqueda.priceRange[0] <= data.productPrice && busqueda.priceRange[1] >= data.productPrice))){
+                let resObj = data;
+                resObj.id = doc.id;
+                promises.push(
+                  resize(data.productImages[0], 480).then(
+                    (img)=>{
+                      resObj.productImages[0] = img
+                      DatosFiltrados.push(resObj)
+                    }
+                  ).catch((e)=>{console.log(e); response.send(undefined);})
+                )
+                
+              }
+            }
+          ))
+          Promise.all(promises).then(
+            (e)=>resolve()
+          ).catch(e=>{console.log(e); response.send(undefined)});
+        }
+      ).then(
+        () => response.send(DatosFiltrados)
+      ).catch(e=>{console.log(e); response.send(undefined)});
+    }
+  ).catch(e=>{console.log(e); response.send(undefined)})
+
+})
+
+function filter(obj:any, filters:any[], busqueda:any){
+  let m = true;
+  filters.forEach(element => {
+    if(m && element != "priceRange" && element != "productSearch" && obj[element] != busqueda[element]){
+      m = false;
+    }
+  });
+  return m;
+}
 
 export const search = functions.https.onRequest((request, response)=>{
   response.set('Access-Control-Allow-Origin', '*');
@@ -66,9 +192,9 @@ export const search = functions.https.onRequest((request, response)=>{
         }
       ).then(
         () => response.send(DatosFiltrados)
-      ).catch(e=>{console.log(e); response.send("Error :" + e)});
+      ).catch(e=>{console.log(e); response.send(undefined)});
     }
-  ).catch(e=>{console.log(e); response.send("Error :" + e)})
+  ).catch(e=>{console.log(e); response.send(undefined)})
 
 })
 
@@ -96,9 +222,9 @@ export const searchtest = functions.https.onRequest((request, response)=>{
         }
       ).then(
         () => response.send(DatosFiltrados)
-      ).catch(e=>{console.log(e); response.send("Error :" + e)});
+      ).catch(e=>{console.log(e); response.send(undefined)});
     }
-  ).catch(e=>{console.log(e); response.send("Error :" + e)})
+  ).catch(e=>{console.log(e); response.send(undefined)})
 
 })
 
--- src/Components/PhoneCanvas.js
+++ src/Components/PhoneCanvas.js
@@ -9,15 +9,22 @@ class PhoneCanvas extends React.Component{
         super(props)
         this.state = {
             widgets: [],
+            scale: [1, 1],
             loaded: false
         }
         this.canvas = React.createRef();
     }
 
+    changeScale = (e)=>{
+        this.setState({scale: [this.state.scale[0] + ((e.nativeEvent.deltaY>0) ? .1 : -.1), this.state.scale[1] + ((e.nativeEvent.deltaY>0) ? .1 : -.1)]})
+        this.paint()
+    }
+
     render(){
+        if(this.canvasContext != undefined) this.paint();
         return (
-            <div width={this.props.width ?? 700} height={this.props.height ?? 700} className="phone-canvas_Container">
-                <canvas ref={this.canvas} className="phone-canvas" width={this.props.width ?? 700} height={this.props.height ?? 700}>
+            <div id="canvas" onWheel={this.changeScale} style={this.props.style} width={this.props.width ?? 700} height={this.props.height ?? 700} className="phone-canvas_Container">
+                <canvas ref={this.canvas} className="phone-canvas" style={this.props.style}>
                 </canvas>
                 {!this.state.loaded && <LoadingPhone className="loadingPhone"></LoadingPhone>}
             </div>
@@ -26,27 +33,66 @@ class PhoneCanvas extends React.Component{
 
     async componentDidMount(){
         this.canvasContext = this.canvas.current.getContext('2d')
-        this.paint();
+        this.canvas.current.width = this.canvas.current.offsetWidth;
+        this.canvas.current.height = this.canvas.current.offsetHeight;
+
+        this.canvasContext.center = [this.canvas.current.offsetWidth / 2, this.canvas.current.offsetHeight / 2]
         let img;
         await fetch(this.props.mask)
         .then(response => response.blob())
         .then(images => img = images)
-        await blobToBase64(img).then(data => {this.mask = new Image(); this.mask.onload = this.paint(); this.mask.src = data;})
-        console.log(this.mask)
+        await blobToBase64(img).then(data => {
+            this.mask = new Image(); 
+            this.mask.onload = () => {
+                this.mask.position = [0,0]
+                this.mask.scale = [1,1]
+                this.paint();
+            };
+            this.mask.src = data;
+        })
         await fetch(this.props.case)
         .then(response => response.blob())
         .then(images => img = images)
-        await blobToBase64(img).then(data => {this.case = new Image(); this.case.onload = this.paint(); this.case.src = data;})
-        this.paint()
-        this.setState({loaded:true})
+        await blobToBase64(img).then(data => {
+            this.case = new Image(); 
+            this.case.onload = ()=>{
+                this.case.position = [0,0]
+                this.case.scale = [1,1]
+                this.paint(); 
+            };
+            this.case.src = data;
+        });
+        this.paint();
+        this.setState({loaded:true});
     }
-
+    
     paint(){
         if(this.canvasContext){
             this.canvasContext.fillStyle = 'rgb(160, 160, 160)';
             this.canvasContext.fillRect(0, 0, this.canvas.current.width, this.canvas.current.width);
-            this.mask && this.canvasContext.drawImage(this.mask,0,0);
-            this.case && this.canvasContext.drawImage(this.case,0,0);
+            this.canvasContext.moveTo(Math.floor(this.canvas.current.width / 2), Math.floor(this.canvas.current.height / 2))
+            this.mask && this.drawImage(this.mask,this.mask.position, this.multiply2x1(this.mask.scale, this.state.scale));
+            this.case && this.drawImage(this.case,this.case.position, this.multiply2x1(this.case.scale, this.state.scale));
+        }
+    }
+
+    multiply2x1(a, b){
+        if(!a || !b)
+            return[0,0]
+        return [a[0] * b[0], a[1] * b[1]]
+    }
+
+    drawImage(img, position, size){
+        if (!position)
+            return this.canvasContext.drawImage(img, this.canvasContext.center[0] - img.width / 2, this.canvasContext.center[1] - img.height / 2);
+        
+        if(!size){
+            this.canvasContext.drawImage(img, this.canvasContext.center[0] + position[0] - img.width / 2, this.canvasContext.center[1] + position[1] - img.width / 2)
+        }else{
+            let realsize = {width: img.width * size[0], height: img.height * size[1]}
+            this.canvasContext.drawImage(img, 
+                this.canvasContext.center[0] + position[0]*size[0] - realsize.width / 2, this.canvasContext.center[1] + position[1]*size[1] - realsize.height / 2,
+                realsize.width, realsize.height)
         }
     }
 }
